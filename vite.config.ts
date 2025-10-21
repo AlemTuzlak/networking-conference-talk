b83547c63bb68f6bb3d054e21cfe5909d7da18c0
Alem Tuzlak <t.zlak@hotmail.com>
2025-10-21 15:01:46 +0200

    remove extra dep

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
-import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
-  plugins: [tailwindcss(), tsconfigPaths()],
+  plugins: [tailwindcss()],
   server: { open: true }
 })
